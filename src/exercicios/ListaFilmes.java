241f3f0f54ee00366b92833bc66693a6a2a65230
athos-vieira <avas@cesar.school>
2026-04-23 20:57:17 +0000

    novo exercicio

---
+++ src/exercicios/ListaFilmes.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class ListaFilmes {
+    public static void main(String[] args) {
+
+        List<String> filmes = new ArrayList<>();
+
+        filmes.add("O poderoso chefão");
+        filmes.add("Matrix");
+        filmes.add("Interstellar");
+
+        //printa a lista de filmes que estão dentro do ArrayList em ordem de indice
+        System.out.println("Lista de filmes:");
+        for (String filme : filmes) {
+            System.out.println(filme);
+        }
+
+        String filmeBuscado = "Matrix";
+
+        if (filmes.contains(filmeBuscado)){
+            System.out.println(filmeBuscado + " está na lista");
+        } else {
+            System.out.println(filmeBuscado + " não está na lista");
+        }
+    }
+}
