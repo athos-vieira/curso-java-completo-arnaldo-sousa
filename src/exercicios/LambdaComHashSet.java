f2c442833eaff6993a6e8fe154513b79202e396e
athos-vieira <avas@cesar.school>
2026-04-24 18:36:46 +0000

    novo exercicio

---
+++ src/exercicios/LambdaComHashSet.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class LambdaComHashSet {
+    public static void main(String[] args) {
+
+        Set<Integer> numeros = new HashSet<>();
+        numeros.add(1);
+        numeros.add(2);
+        numeros.add(3);
+        numeros.add(4);
+        numeros.add(5);
+        numeros.add(6);
+        numeros.add(7);
+
+        numeros.removeIf( n -> n % 2 == 0);
+        System.out.println("Numeros ímpares:" + numeros);
+    }
+}       
